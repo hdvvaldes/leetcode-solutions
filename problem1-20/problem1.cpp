4a31354b88c3bb5af304b9a52fbf9e150c2fefed
hectorvvv <hectorvaldes245@gmail.com>
2024-08-01 19:41:03 -0600

    Problem1

---
+++ problem1-20/problem1.cpp
@@ -0,0 +1,18 @@
+#include <unordered_map>
+#include <vector>
+class Solution {
+    public:
+        std::vector<int> twoSum (std::vector<int> &nums, int target) {
+            std::unordered_map<int, int> myMap;
+            for(int i = 0; i < nums.size(); ++i) {
+                if(myMap.find(target-nums[i]) != myMap.end())
+                    return std::vector<int> {i, myMap[target - nums[i]]};
+                myMap[nums[i]] = i;
+            }
+            return std::vector<int> {};
+        }
+        
+
+
+
+};
